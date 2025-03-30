c88df0079ff3a139a4c63d9db3cdda8e5e112f6c
chstinson <130961221+chstinson@users.noreply.github.com>
2025-03-30 19:13:48 +0200

    Update formatters.js

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,14 +1,37 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ZinsCalculator from './ZinsCalculator';
-import './index.css';
+/**
+ * Formatiert einen Wert als Währung (EUR)
+ */
+export const formatCurrency = (value) => {
+  return new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR'
+  }).format(value);
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div className="container mx-auto py-8">
-      <ZinsCalculator />
-    </div>
-  </React.StrictMode>
-);
+/**
+ * Formatiert einen Wert als Prozent
+ */
+export const formatPercent = (value) => {
+  return new Intl.NumberFormat('de-DE', {
+    style: 'percent',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(value / 100);
+};
+
+/**
+ * Formatiert einen Wert als Zahl
+ */
+export const formatNumber = (value) => {
+  return new Intl.NumberFormat('de-DE').format(value);
+};
+
+/**
+ * Formatiert einen Monatswert als Quartal
+ */
+export const formatQuartal = (monat) => {
+  if (monat === 0) return "Initial";
+  const jahr = Math.floor(monat / 12) + 1;
+  const quartal = Math.ceil((monat % 12) / 3);
+  return `Jahr ${jahr}, Q${quartal}`;
+};
